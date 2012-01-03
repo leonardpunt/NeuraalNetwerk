85202b7e8be8855815d898d45886c7bd9cfcdbf1
Leonard Punt <leonardpunt@gmail.com>
2012-01-03 14:11:21 +0100

    setup

---
+++ src/Run.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+
+import mnist.tools.MnistManager;
+
+public class Run {
+
+	public static void main(String[] args) {
+		MnistManager m;
+		try {
+			m = new MnistManager("data/t10k-images-idx3-ubyte",
+					"data/t10k-labels-idx1-ubyte");
+			m.setCurrent(10); // index of the image that we are interested in
+			int[][] image = m.readImage();
+			
+			System.out.println("Label:" + m.readLabel());
+			new File("output").mkdir();
+			MnistManager.writeImageToPpm(image, "output/10.ppm");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
