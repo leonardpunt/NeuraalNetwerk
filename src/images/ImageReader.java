61ba5e44e3fc9c5b62e86e68b66a1c1f6c9746b2
Albert Groothedde <albertgroothedde@gmail.com>
2012-01-17 15:03:47 +0100

    Methode imageReader gemaakt. Dit is incl omzetten naar double tussen -1 (wit) en 1 (zwart)

--- src/images/ImageReader.java
+++ src/images/ImageReader.java
@@ -7,19 +7,50 @@ import mnist.tools.MnistManager;
 
 public class ImageReader {
 
-	public void test() {
-		MnistManager m;
-		try {
-			m = new MnistManager("data/t10k-images-idx3-ubyte",
-					"data/t10k-labels-idx1-ubyte");
-			m.setCurrent(10); // index of the image that we are interested in
-			int[][] image = m.readImage();
-			
-			System.out.println("Label:" + m.readLabel());
-			new File("output").mkdir();
-			MnistManager.writeImageToPpm(image, "output/10.ppm");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    MnistManager m;
+
+    public ImageReader() {
+        try {
+            this.m = new MnistManager("data/t10k-images-idx3-ubyte",
+                    "data/t10k-labels-idx1-ubyte");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void test() {
+        MnistManager m;
+        try {
+            m = new MnistManager("data/t10k-images-idx3-ubyte",
+                    "data/t10k-labels-idx1-ubyte");
+            m.setCurrent(10); // index of the image that we are interested in
+            int[][] image = m.readImage();
+
+            System.out.println("Label:" + m.readLabel());
+            new File("output").mkdir();
+            MnistManager.writeImageToPpm(image, "output/10.ppm");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int[][] readImage(int i) {
+        m.setCurrent(i); // index of the image that we are interested in
+        int[][] image = new int[28][28];
+        int[][] newImage = new int[28][28];
+
+        try {
+            image = m.readImage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int j = 0; j < image.length; j++) {
+            for (int k = 0; k < image[j].length; k++) {
+                newImage[j][k] = (image[j][k]/128)-1;
+            }
+        }
+
+        return newImage;
+    }
 }
