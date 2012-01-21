62961526115c4beb890a4f898db2fb567d168403
highmastdon <albertgroothedde@gmail.com>
2012-01-21 22:42:00 +0100

    Optimized with threaded ImageReader (especially this one is what makes it faster). It's done ugly without any loops for the amount of cores. It's now just 4 threaded.

--- src/Run.java
+++ src/Run.java
@@ -1,3 +1,4 @@
+
 import images.ImageReader;
 
 import java.util.Calendar;
@@ -12,141 +13,232 @@ import neuraalnetwork.Neuron;
 
 public class Run {
 
-	public static void main(String[] args) {
-		Date timeStarted = Calendar.getInstance().getTime();
-		System.out.println("--- Started at " + timeStarted + " ---");
-		
-		ImageReader ir = new ImageReader();
+    static void threadMessage(String message) {
+        String threadName =
+                Thread.currentThread().getName();
+        System.out.format("%s: %s%n",
+                threadName,
+                message);
+    }
 
-		/*
-		 * RESULTS
-		 * 
-		 * numberNeuronsHiddenLayer : 10 0.008 = 27%; 23%
-		 * 
-		 * numberNeuronsHiddenLayer : 50 0.05 = 51%; 53% 0.03 = 60%; 57% 0.01 =
-		 * 53%; 63%; 59% 0.008 = 57%; 65%; 66%; 63% 0.005 = 68%; 67%; 57%
-		 * 
-		 * numberNeuronsHiddenLayer : 70 0.008 = 57%; 58
-		 * 
-		 * numberNeuronsHiddenLayer : 100 0.008 = 40%; 57%; 56%
-		 * 
-		 * numberNeuronsHiddenLayer : 150 0.008 = 53%
-		 */
-		int numberNeuronsHiddenLayer = 10;
-		double learningRate = 0.008;
+    private static class BackPropagateThread implements Runnable {
 
-		// Initialize network
-		NeuralNetwork nn = new NeuralNetwork(numberNeuronsHiddenLayer);
+        int from, to;
+        double learningRate;
+        NeuralNetwork nn;
 
-		// Get validation set
-		int sizeValidationSet = 50;
-		List<Integer> indicesValidationSet = new LinkedList<Integer>();
-		for (int i = 1; i <= sizeValidationSet; i++) {
-			indicesValidationSet.add((int) (Math.random() * ir
-					.lengthOfTrainingSet()));
-		}
+        // Constructor for BackPropagateThread since you can't give
+        // run() any parameters.
+        public BackPropagateThread(int from, int to, double learningRate, NeuralNetwork nn) {
+            this.from = from;
+            this.to = to;
+            this.nn = nn;
+            this.learningRate = learningRate;
+        }
 
-		// Train and validate network
-		boolean train = true;
-		int lastNumberOfRightAnswers = 0;
-		while (train) {		
-			
-			// Train
-			Date trainingsRoundStarted = Calendar.getInstance().getTime();
-			for (int i = 1; i <= ir.lengthOfTrainingSet(); i++) {
-				double[] image = ir.readImage(i, ir.getTrainingSet());
-				int label = ir.readLabel(i, ir.getTrainingSet());
-				double[] actualOutput = nn.forwardPropagate(image);
-				nn.backPropagate(actualOutput, getTargetOutput(label),
-						learningRate);
-			}
-			
-			// Validate
-			int numberOfRightAnswers = 0;
-			for (int i = 0; i < indicesValidationSet.size(); i++) {
-				double[] image = ir.readImage(indicesValidationSet.get(i), ir.getTrainingSet());
-				int label = ir.readLabel(indicesValidationSet.get(i), ir.getTrainingSet());
-				double[] actualOutput = nn.forwardPropagate(image);
-				
-				double bestOutput = -100.0;
-				int bestIndex = 0;
-				for (int j = 0; j < actualOutput.length; j++) {
-					if (actualOutput[j] > bestOutput) {
-						bestOutput = actualOutput[j];
-						bestIndex = j;
-					}
-				}
-				
-				if (label == bestIndex)
-					numberOfRightAnswers++;
-			}
-			
-			Date trainingsRoundEnded = Calendar.getInstance().getTime();
-			System.out.println("Current error: " + (sizeValidationSet - numberOfRightAnswers) +
-					"; Round time: " + ((trainingsRoundEnded.getTime() - trainingsRoundStarted.getTime()) / 1000) + " seconds");
-			
-			// Enough trained?					
-			if (numberOfRightAnswers < lastNumberOfRightAnswers)
-				train = false;
-			else
-				lastNumberOfRightAnswers = numberOfRightAnswers;
-		}
+        public void run() {
+            threadMessage("Thread is starting: from "
+                    + from
+                    + " to "
+                    + to);
+            // ImageReader is 
+            ImageReader ir = new ImageReader();
+            for (int i = this.from + 1; i < this.to; i++) {
+                double[] image = ir.readImage(i, ir.getTrainingSet());
+                int label = ir.readLabel(i, ir.getTrainingSet());
+                double[] actualOutput = nn.forwardPropagate(image);
+                nn.backPropagate(actualOutput, getTargetOutput(label),
+                        learningRate);
+                //threadMessage("reports last: " + i);
+            }
+            threadMessage("Thread ended");
+        }
+    }
 
-		// Test network
-		int numberOfRightAnswers = 0;
-		for (int i = 1; i <= ir.lengthOfTestSet(); i++) {
-			double[] image = ir.readImage(i, ir.getTestSet());
-			int label = ir.readLabel(i, ir.getTestSet());
-			double[] actualOutput = nn.forwardPropagate(image);
+    public static void main(String[] args) {
+        Date timeStarted = Calendar.getInstance().getTime();
+        System.out.println("--- Started at " + timeStarted + " ---");
 
-			double bestOutput = -100.0;
-			int bestIndex = 0;
-			for (int j = 0; j < actualOutput.length; j++) {
-				if (actualOutput[j] > bestOutput) {
-					bestOutput = actualOutput[j];
-					bestIndex = j;
-				}
-			}
+        ImageReader ir = new ImageReader();
 
-			if (label == bestIndex)
-				numberOfRightAnswers++;
-		}
+        /*
+         * RESULTS
+         * 
+         * numberNeuronsHiddenLayer : 10 0.008 = 27%; 23%
+         * 
+         * numberNeuronsHiddenLayer : 50 
+         * 0.05 = 51%; 53% 
+         * 0.03 = 60%; 57% 
+         * 0.01 = 53%; 63%; 59% 
+         * 0.008 = 57%; 65%; 66%; 63% 
+         * 0.005 = 68%; 67%; 57%
+         * 
+         * numberNeuronsHiddenLayer : 70 
+         * 0.008 = 57%; 58
+         * 
+         * numberNeuronsHiddenLayer : 100 
+         * 0.008 = 40%; 57%; 56%
+         * 
+         * numberNeuronsHiddenLayer : 150 
+         * 0.008 = 53%
+         * 
+         * numberNeuronsHiddenLayer = 180;
+         * 0.00005 = 18.66%
+         * 0.0005 = 16.55%
+         */
+        int numberNeuronsHiddenLayer = 50;
+        double learningRate = 0.005;
 
-		System.out.println("Found " + numberOfRightAnswers + " right answers in " + ir.lengthOfTestSet() + " tests");
-		System.out.println("Accuracy: " + (double) numberOfRightAnswers	/ (double) ir.lengthOfTestSet() * 100 + "%");
-		
-		Date timeFinished = Calendar.getInstance().getTime();
-		System.out.println("--- Ended at " + timeFinished + " --- " +
-				"Runtime: " + ((timeFinished.getTime() - timeStarted.getTime()) / 1000) + " seconds");
-	}
+        // Initialize network
+        NeuralNetwork nn = new NeuralNetwork(numberNeuronsHiddenLayer);
 
-	private static double[] getTargetOutput(int label) {
-		double[] targetOutput = new double[10];
-		for (int i = 0; i < targetOutput.length; i++) {
-			targetOutput[i] = -1.0;
-		}
-		targetOutput[label] = 1.0;
-		return targetOutput;
-	}
+        // Get validation set
+        int sizeValidationSet = 1000;
+        List<Integer> indicesValidationSet = new LinkedList<Integer>();
+        for (int i = 1; i <= sizeValidationSet; i++) {
+            indicesValidationSet.add((int) (Math.random() * ir.lengthOfTrainingSet()));
+        }
 
-	private static void printNetwork(NeuralNetwork nn) {
-		int layerIndex = 0;
-		for (Layer layer : nn.getLayers().subList(2, 3)) {
-			System.out.println("Layer " + layerIndex);
-			layerIndex++;
+        // Train and validate network
+        boolean train = true;
+        int lastNumberOfRightAnswers = 0;
+        while (train) {
 
-			int neuronIndex = 0;
-			for (Neuron neuron : layer.getNeurons()) {
-				System.out.println("Neuron " + neuronIndex + " Output: "
-						+ neuron.getOutput());
-				neuronIndex++;
+            Date trainingsRoundStarted = Calendar.getInstance().getTime();
 
-				int connectionIndex = 0;
-				for (Connection connection : neuron.getConnections()) {
-					// System.out.println("Connection " + connectionIndex + " Weight: " + connection.getWeight().getValue());
-					connectionIndex++;
-				}
-			}
-		}
-	}
+            // Train
+            // Starting up 4 threads (4-core CPU)
+            // Still ugly as no loops are used to built and start the threads
+
+            Thread t0 = new Thread(new BackPropagateThread(0, (ir.lengthOfTrainingSet() / 4) * 1, learningRate, nn));
+            Thread t1 = new Thread(new BackPropagateThread((ir.lengthOfTrainingSet() / 4) * 1 + 1, (ir.lengthOfTrainingSet() / 4) * 2, learningRate, nn));
+            Thread t2 = new Thread(new BackPropagateThread((ir.lengthOfTrainingSet() / 4) * 2 + 1, (ir.lengthOfTrainingSet() / 4) * 3, learningRate, nn));
+            Thread t3 = new Thread(new BackPropagateThread((ir.lengthOfTrainingSet() / 4) * 3 + 1, (ir.lengthOfTrainingSet() / 4) * 4, learningRate, nn));
+
+            threadMessage("initiating start to 4 threads");
+            t0.start();
+            t1.start();
+            t2.start();
+            t3.start();
+
+            try {
+                threadMessage("waiting to continue for " + t0.getName());
+                t0.join();
+                Thread.sleep(100); // After including these sleeps I din't get some nullpointer exceptions
+                threadMessage("waiting to continue for " + t1.getName());
+                t1.join();
+                Thread.sleep(100);
+                threadMessage("waiting to continue for " + t2.getName());
+                t2.join();
+                Thread.sleep(100);
+                threadMessage("waiting to continue for " + t3.getName());
+                t3.join();
+
+            } catch (Exception e) {
+                System.err.println("Something went wrong");
+                System.err.println(e.toString());
+            }
+
+//                        Date trainingsRoundStarted = Calendar.getInstance().getTime();
+//                        for (int i = 1; i <= ir.lengthOfTrainingSet(); i++) {
+//                                double[] image = ir.readImage(i, ir.getTrainingSet());
+//                                int label = ir.readLabel(i, ir.getTrainingSet());
+//                                double[] actualOutput = nn.forwardPropagate(image);
+//                                nn.backPropagate(actualOutput, getTargetOutput(label),
+//                                        learningRate);
+//                        }
+
+            // Validate
+            int numberOfRightAnswers = 0;
+            for (int i = 0; i < indicesValidationSet.size(); i++) {
+                double[] image = ir.readImage(indicesValidationSet.get(i), ir.getTrainingSet());
+                int label = ir.readLabel(indicesValidationSet.get(i), ir.getTrainingSet());
+                double[] actualOutput = nn.forwardPropagate(image);
+
+                double bestOutput = -100.0;
+                int bestIndex = 0;
+                for (int j = 0; j < actualOutput.length; j++) {
+                    if (actualOutput[j] > bestOutput) {
+                        bestOutput = actualOutput[j];
+                        bestIndex = j;
+                    }
+                }
+
+                if (label == bestIndex) {
+                    numberOfRightAnswers++;
+                }
+            }
+            Date trainingsRoundEnded = Calendar.getInstance().getTime();
+            double percentage = ((double) numberOfRightAnswers / (double) sizeValidationSet) * 100;
+            System.out.println("Current success rate: " + numberOfRightAnswers + "/" + sizeValidationSet + " = " + (int) percentage + "%");
+            System.out.println("Round time: " + ((trainingsRoundEnded.getTime() - trainingsRoundStarted.getTime()) / 1000) + " seconds");
+//                        System.out.println("Current error: " + (sizeValidationSet - numberOfRightAnswers)
+//                                + "; Round time: " + ((trainingsRoundEnded.getTime() - trainingsRoundStarted.getTime()) / 1000) + " seconds");
+
+            // Enough trained?					
+            if (numberOfRightAnswers < lastNumberOfRightAnswers) {
+                train = false;
+            } else {
+                lastNumberOfRightAnswers = numberOfRightAnswers;
+            }
+        }
+
+        // Test network
+        int numberOfRightAnswers = 0;
+        for (int i = 1; i <= ir.lengthOfTestSet(); i++) {
+            double[] image = ir.readImage(i, ir.getTestSet());
+            int label = ir.readLabel(i, ir.getTestSet());
+            double[] actualOutput = nn.forwardPropagate(image);
+
+            double bestOutput = -100.0;
+            int bestIndex = 0;
+            for (int j = 0; j < actualOutput.length; j++) {
+                if (actualOutput[j] > bestOutput) {
+                    bestOutput = actualOutput[j];
+                    bestIndex = j;
+                }
+            }
+
+            if (label == bestIndex) {
+                numberOfRightAnswers++;
+            }
+        }
+
+        System.out.println("Found " + numberOfRightAnswers + " right answers in " + ir.lengthOfTestSet() + " tests");
+        System.out.println("Accuracy: " + (double) numberOfRightAnswers / (double) ir.lengthOfTestSet() * 100 + "%");
+
+        Date timeFinished = Calendar.getInstance().getTime();
+        System.out.println("--- Ended at " + timeFinished + " --- "
+                + "Runtime: " + ((timeFinished.getTime() - timeStarted.getTime()) / 1000) + " seconds");
+    }
+
+    private static double[] getTargetOutput(int label) {
+        double[] targetOutput = new double[10];
+        for (int i = 0; i < targetOutput.length; i++) {
+            targetOutput[i] = -1.0;
+        }
+        targetOutput[label] = 1.0;
+        return targetOutput;
+    }
+
+    private static void printNetwork(NeuralNetwork nn) {
+        int layerIndex = 0;
+        for (Layer layer : nn.getLayers().subList(2, 3)) {
+            System.out.println("Layer " + layerIndex);
+            layerIndex++;
+
+            int neuronIndex = 0;
+            for (Neuron neuron : layer.getNeurons()) {
+                System.out.println("Neuron " + neuronIndex + " Output: "
+                        + neuron.getOutput());
+                neuronIndex++;
+
+                int connectionIndex = 0;
+                for (Connection connection : neuron.getConnections()) {
+                    // System.out.println("Connection " + connectionIndex + " Weight: " + connection.getWeight().getValue());
+                    connectionIndex++;
+                }
+            }
+        }
+    }
 }
